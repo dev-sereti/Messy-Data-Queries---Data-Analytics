4eacee6d3be7db9989be6cdf2f5ba14d44a48016
dev-sereti <devsereti@gmail.com>
2026-03-12 12:02:10 +0300

    Query added.

---
+++ clean_practise.sql
@@ -0,0 +1,61 @@
+--Explore tables
+--products
+--orders
+--order_items
+--employees
+
+select * from customers;
+select * from products;
+select * from orders;
+select * from order_items;
+select * from employees;
+
+
+
+select * from customers
+where signup_date > '2022-01-01';
+
+select * from products
+order by price desc ;
+
+select COUNT(*)
+from customers;
+
+select category,count(*)
+from products
+group by category;
+
+select * from orders o 
+where o.total_amount is null;
+
+--
+--Cleaning , Transformation 
+
+--Find duplicates customers 
+select email, count(*)
+from customers c 
+group by c.email 
+having count(*) >1;
+
+select distinct order_status
+from orders;
+
+update orders o 
+set order_status = TRIM(initcap(order_status));
+
+select distinct city 
+from customers c ;
+
+update customers c 
+set city = case
+	when city in ('new york')		then 'New York'
+	when city in ('New Yoyk')		then 'New York'
+	when city in ('Chciago')		then 'Chicago'
+	when c.city in ('Los angeles') 	then 'Los Angeles'
+end
+where c.city in ('new york','Chciago','Los angeles','New Yoyk');
+
+update customers c 
+set 
+first_name = initcap(c.first_name ),
+last_name = initcap(c.last_name );
